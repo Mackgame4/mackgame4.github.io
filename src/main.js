8ae0ae4914c927e8f383af438566163e7bda54b7
Mackgame4 <42896437+Mackgame4@users.noreply.github.com>
2022-07-04 19:46:26 +0100

    + Files

---
+++ src/main.js
@@ -0,0 +1,52 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', component: App }
+    ]
+});
+
+import PrimeVue from 'primevue/config';
+//import 'primevue/resources/themes/lara-dark-purple/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
+const app = createApp(App)
+app.use(router)
+
+app.mixin({
+    data: () => ({
+        discordLink: 'https://discord.gg/Vw6acJ5',
+        githubLink: 'https://github.com/Mackgame4',
+        twitterLink: 'https://twitter.com'
+    }),
+    methods: {
+        openInNewTab(url) {
+            window.open(url, '_blank');
+        }
+    }
+})
+
+app.use(PrimeVue);
+import Dialog from 'primevue/dialog';
+app.component('Dialog', Dialog);
+import Button from 'primevue/button';
+app.component('Button', Button);
+import ToggleButton from 'primevue/togglebutton';
+app.component('ToggleButton', ToggleButton);
+import InputSwitch from 'primevue/inputswitch';
+app.component('InputSwitch', InputSwitch);
+import Avatar from 'primevue/avatar';
+app.component('Avatar', Avatar);
+import Sidebar from 'primevue/sidebar';
+app.component('Sidebar', Sidebar);
+import MegaMenu from 'primevue/megamenu';
+app.component('MegaMenu', MegaMenu);
+import Card from 'primevue/card';
+app.component('Card', Card);
+
+app.mount('#app')
---
+++ src/pages/About.vue
@@ -0,0 +1,111 @@
+<template>
+    <div id="aboutPage">
+        <h1 style="margin-top: -5px;">About</h1>
+        <Markdown id="markdownContainer" :source="readmeSource" html :breaks="false" linkify />
+        <br>
+        <Button label="Follow me!" icon="pi pi-github" class="p-button-rounded p-button-text" @click="openInNewTab(githubLink)" />
+        <Button label="Contact me!" icon="pi pi-discord" class="p-button-rounded p-button-text" @click="openInNewTab(discordLink)" />
+    </div>
+</template>
+
+<script>
+import Markdown from 'vue3-markdown-it';
+
+export default {
+    name: 'App',
+    components: {
+        Markdown
+    },
+    data() {
+        return {
+        }
+    },
+    mounted() {
+        this.fetchDiscordUser();
+    },
+    computed: {
+        readmeSource() {
+            let url = 'https://raw.githubusercontent.com/Mackgame4/Mackgame4/master/README.md';
+            let request = new XMLHttpRequest();
+            request.open('GET', url, false);
+            request.send(null);
+            return request.responseText;
+        }
+    },
+    methods: {
+        fetchDiscordUser() {
+            // https://github.com/chocololat/discord-lookup-api
+            let url = 'https://discordlookup.mesavirep.xyz/263676903116898304';
+            let request = new XMLHttpRequest();
+            request.open('GET', url, false);
+            request.send(null);
+            let response = JSON.parse(request.responseText);
+            var waitForElement = setInterval(() => {
+                if (document.getElementById("discord")) {
+                    document.getElementById("discord").innerHTML = response.tag;
+                    var list = document.querySelectorAll('a');
+                    for (let i = 0; i < list.length; i++) {
+                        list[i].setAttribute('target', '_blank');
+                    }
+                    clearInterval(waitForElement);
+                }
+            }, 100);
+        },
+    }
+}
+</script>
+
+<style>
+a:link {
+    color: var(--primary-color);
+    background-color: transparent;
+    text-decoration: none;
+}
+a:visited {
+    color: var(--primary-color);
+    background-color: transparent;
+    text-decoration: none;
+}
+a:hover {
+    color: pink;
+    background-color: transparent;
+    text-decoration: underline;
+}
+a:active {
+    color: pink;
+    background-color: transparent;
+    text-decoration: underline;
+}
+
+#markdownContainer {
+    margin-top: 20px;
+}
+
+#markdownContainer h4 {
+    max-width: 980px;
+    margin-left: auto;
+    margin-right: auto;
+}
+
+#markdownContainer p {
+    margin-top: -5px;
+    display: inline-block;
+    text-align: center;
+}
+
+#markdownContainer ul {
+    list-style-type: none;
+}
+
+/* https://codepen.io/sosuke/pen/Pjoqqp */
+#markdownContainer p a .icon {
+    margin: 5px;
+    margin-right: 10px;
+    filter: invert(67%) sepia(35%) saturate(580%) hue-rotate(208deg) brightness(105%) contrast(98%);
+}
+
+#markdownContainer p a .icon:hover {
+    filter: invert(76%) sepia(9%) saturate(1018%) hue-rotate(301deg) brightness(103%) contrast(106%);
+}
+
+</style>
