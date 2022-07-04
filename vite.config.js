ea62b547dfecc8cd326777e5f00f97705e145c9f
Mackgame4 <42896437+Mackgame4@users.noreply.github.com>
2022-07-04 20:03:30 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path';
+import path from 'path'
 import vue from '@vitejs/plugin-vue'
 
+const baseUrl = {
+  development: './',
+  beta: './',
+  release: './'
+}
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default ({ mode }) =>  defineConfig({
+  base: baseUrl[mode],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
-    },
+      '~': path.resolve(__dirname, './'),
+      '@': path.resolve(__dirname, 'src')
+    }
   },
+  plugins: [vue()]
 })
